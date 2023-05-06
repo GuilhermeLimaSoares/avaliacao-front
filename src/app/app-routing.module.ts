022e5324d9e002b1e8d7ca1f378e8728338ff975
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-05-06 09:11:25 -0300

    Create initial routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './src/pages/create-user/create-user.component';
+import { UserListComponent } from './src/pages/user-list/user-list.component';
+import { UserEditComponent } from './src/pages/user-edit/user-edit.component';
+import { UserDetailComponent } from './src/pages/user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'create-user',
+    component: CreateUserComponent,
+  },
+  {
+    path: 'users',
+    component: UserListComponent,
+  },
+    {
+    path: 'users/detail-user/:id',
+    component: UserDetailComponent,
+  },
+  // {
+  //   path: 'users/delete-user/:id',
+  //   component: UserDeleteComponent,
+  // },
+  {
+    path: 'users/edit-user/:id',
+    component: UserEditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
