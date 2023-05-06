033892e25c5c848709b7ecb3da3d568b7229b9b2
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-05-06 08:56:21 -0300

    Create initial page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './src/pages/user-list/user-list.component';
+import { UserDetailComponent } from './src/pages/user-detail/user-detail.component';
+import { UserEditComponent } from './src/pages/user-edit/user-edit.component';
+import { CreateUserComponent } from './src/pages/create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
