31af8facb143336b1e3fd650bb20c83fa2a834a3
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-05-06 08:47:15 -0300

    Add initial enviroment config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  token: '6452a8908edea0eafd918058'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  token: '6452a8908edea0eafd918058'
+};
